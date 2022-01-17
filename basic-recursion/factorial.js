faafeb5e91dd49c2ba207a8a9848eb714ee97f9c
Jay Palacio <jaypalacio@gmail.com>
2022-01-17 10:49:22 -0500

    some simple recursion practice

---
+++ basic-recursion/factorial.js
@@ -0,0 +1,10 @@
+// factorial (1) // 1
+// factorial (2) // 2
+// factorial (4) // 24
+
+function factorial(num) {
+    if (num === 0) return 1;
+    return num * factorial(num - 1);
+}
+
+console.log(factorial(4), " 24");
